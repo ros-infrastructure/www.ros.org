2fba273028a2607130931d0dc65b4e852e8b98e5
Andrew Snider <andrewsnider15@gmail.com>
2021-04-28 22:47:18 -0700

    Finish desktop version 1.0 of navbar

--- content/javascript/default.js
+++ content/javascript/default.js
@@ -5,24 +5,57 @@ function openCloseDotsMenu() {
   let classes = dots.classList;
   let dotTopClasses = topDot.classList;
   let dotBottomClasses = bottomDot.classList;
+  let aboutList = document.getElementById("submenu-about");
+  let aboutClasses = aboutList.classList;
 
   if (classes.contains("hidden")) {
     classes.remove("hidden");
     classes.add("show");
 
-    dotTopClasses.add("hidden");
-    dotTopClasses.remove("show");
+    dotTopClasses.add("invisible");
+    dotTopClasses.remove("visible");
 
-    dotBottomClasses.add("hidden");
-    dotBottomClasses.remove("show");
+    dotBottomClasses.add("invisible");
+    dotBottomClasses.remove("visible");
+
+    aboutClasses.add("hidden");
+    aboutClasses.remove("show");
   } else {
     classes.remove("show");
     classes.add("hidden");
 
-    dotTopClasses.add("show");
-    dotTopClasses.remove("hidden");
+    dotTopClasses.remove("invisible");
+    dotTopClasses.add("visible");
 
-    dotBottomClasses.add("show");
-    dotBottomClasses.remove("hidden");
+    dotBottomClasses.add("visible");
+    dotBottomClasses.remove("invisible");
+  }
+}
+
+function openCloseAbout() {
+  let aboutList = document.getElementById("submenu-about");
+  let classes = aboutList.classList;
+  let dots = document.getElementById("submenu-dots");
+  let topDot = document.getElementById("dot-top");
+  let bottomDot = document.getElementById("dot-bottom");
+  let dotsMenuClasses = dots.classList;
+  let dotTopClasses = topDot.classList;
+  let dotBottomClasses = bottomDot.classList;
+
+  if (classes.contains("hidden")) {
+    classes.remove("hidden");
+    classes.add("show");
+
+    dotsMenuClasses.remove("show");
+    dotsMenuClasses.add("hidden");
+
+    dotTopClasses.remove("invisible");
+    dotTopClasses.add("visible");
+
+    dotBottomClasses.remove("invisible");
+    dotBottomClasses.add("visible");
+  } else {
+    classes.remove("show");
+    classes.add("hidden");
   }
 }
