ec5b51190db94d1d119813354d8a48295118aa6d
Andrew Snider <andrewsnider15@gmail.com>
2021-05-03 15:42:39 -0700

    Make dropdown menus close on click outside of element

--- content/javascript/default.js
+++ content/javascript/default.js
@@ -1,3 +1,36 @@
+function closeMenus(menuId, event) {
+  let dots = document.getElementById("submenu-dots");
+  let topDot = document.getElementById("dot-top");
+  let bottomDot = document.getElementById("dot-bottom");
+  let classes = dots.classList;
+  let dotTopClasses = topDot.classList;
+  let dotBottomClasses = bottomDot.classList;
+
+  let aboutList = document.getElementById("submenu-about");
+  let aboutClasses = aboutList.classList;
+
+  // stop checking in parent elements further up the DOM
+  window.event.stopPropagation();
+
+  if (menuId.id == "about") {
+    return;
+  } else if (menuId.id == "dropdown-dots") {
+    return;
+  } else {
+    classes.remove("show");
+    classes.add("hidden");
+
+    dotTopClasses.remove("invisible");
+    dotTopClasses.add("visible");
+
+    dotBottomClasses.remove("invisible");
+    dotBottomClasses.add("visible");
+
+    aboutClasses.add("hidden");
+    aboutClasses.remove("show");
+  }
+}
+
 function openCloseDotsMenu() {
   let dots = document.getElementById("submenu-dots");
   let topDot = document.getElementById("dot-top");
@@ -57,30 +90,5 @@ function openCloseAbout() {
   } else {
     classes.remove("show");
     classes.add("hidden");
-  }
-}
-
-function closeMenus() {
-  let aboutList = document.getElementById("submenu-about");
-  let classes = aboutList.classList;
-  let dots = document.getElementById("submenu-dots");
-  let topDot = document.getElementById("dot-top");
-  let bottomDot = document.getElementById("dot-bottom");
-  let dotsMenuClasses = dots.classList;
-  let dotTopClasses = topDot.classList;
-  let dotBottomClasses = bottomDot.classList;
-
-  if (classes.contains("show") || dotMenuClasses.contains("show")) {
-    classes.remove("show");
-    classes.add("hidden");
-
-    dotsMenuClasses.remove("show");
-    dotsMenuClasses.add("hidden");
-
-    dotTopClasses.remove("invisible");
-    dotTopClasses.add("visible");
-
-    dotBottomClasses.remove("invisible");
-    dotBottomClasses.add("visible");
   }
 }
