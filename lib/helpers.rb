5864a84a4b818e416bf168e8cf7c40afc81a296d
Andrew Snider <andrew@miriamtech.com>
2021-06-25 16:58:14 -0700

    Add helpers

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,2 +1,5 @@
 use_helper Nanoc::Helpers::HTMLEscape
 use_helper Nanoc::Helpers::Rendering
+use_helper Nanoc::Helpers::Blogging
+user_helper Nanoc::Helpers::Tagging
+user_helper Nanoc::Helpers::LinkTo
